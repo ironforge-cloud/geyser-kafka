d14a789567ad926e8d022f9f26683781e780ebba
Thorsten Lorenz <thlorenz@gmx.de>
2023-08-09 10:10:57 +0700

    chore: extract key + buf account update create + add tests

--- src/publisher/kafka_publisher.rs
+++ src/publisher/kafka_publisher.rs
@@ -73,17 +73,8 @@ impl KafkaPublisher {
             .get(&ev.owner)
             .unwrap_or(&self.update_account_topic);
 
-        let temp_key;
-        let (key, buf) = if self.wrap_messages {
-            let key = self.account_update_key(&ev.owner);
-            temp_key = self.copy_and_prepend(key.as_bytes(), 65u8);
-            (&temp_key, Self::encode_with_wrapper(Account(Box::new(ev))))
-        } else {
-            let key = self.account_update_key(&ev.owner);
-            temp_key = key.as_bytes().to_vec();
-            (&temp_key, ev.encode_to_vec())
-        };
-        let record = BaseRecord::<Vec<u8>, _>::to(topic).key(key).payload(&buf);
+        let (key, buf) = Self::account_update_key_and_data(ev, &self.cluster, self.wrap_messages);
+        let record = BaseRecord::<Vec<u8>, _>::to(topic).key(&key).payload(&buf);
         let result = self.producer.send(record).map(|_| ()).map_err(|(e, _)| e);
         UPLOAD_ACCOUNTS_TOTAL
             .with_label_values(&[if result.is_ok() { "success" } else { "failed" }])
@@ -94,7 +85,7 @@ impl KafkaPublisher {
     pub fn update_slot_status(&self, ev: SlotStatusEvent) -> Result<(), KafkaError> {
         let temp_key;
         let (key, buf) = if self.wrap_messages {
-            temp_key = self.copy_and_prepend(&ev.slot.to_le_bytes(), 83u8);
+            temp_key = Self::copy_and_prepend(&ev.slot.to_le_bytes(), 83u8);
             (&temp_key, Self::encode_with_wrapper(Slot(Box::new(ev))))
         } else {
             temp_key = ev.slot.to_le_bytes().to_vec();
@@ -113,7 +104,7 @@ impl KafkaPublisher {
     pub fn update_transaction(&self, ev: TransactionEvent) -> Result<(), KafkaError> {
         let temp_key;
         let (key, buf) = if self.wrap_messages {
-            temp_key = self.copy_and_prepend(ev.signature.as_slice(), 84u8);
+            temp_key = Self::copy_and_prepend(ev.signature.as_slice(), 84u8);
             (
                 &temp_key,
                 Self::encode_with_wrapper(Transaction(Box::new(ev))),
@@ -150,16 +141,34 @@ impl KafkaPublisher {
         .encode_to_vec()
     }
 
-    fn copy_and_prepend(&self, data: &[u8], prefix: u8) -> Vec<u8> {
+    fn account_update_key_and_data(
+        ev: UpdateAccountEvent,
+        cluster: &Cluster,
+        wrap_messages: bool,
+    ) -> (Vec<u8>, Vec<u8>) {
+        if wrap_messages {
+            let key = Self::account_update_key(cluster, &ev.owner);
+            let key = Self::copy_and_prepend(key.as_bytes(), 65u8);
+            let data = Self::encode_with_wrapper(Account(Box::new(ev)));
+            (key, data)
+        } else {
+            let key = Self::account_update_key(cluster, &ev.owner);
+            let key = key.as_bytes().to_vec();
+            let data = ev.encode_to_vec();
+            (key, data)
+        }
+    }
+
+    fn copy_and_prepend(data: &[u8], prefix: u8) -> Vec<u8> {
         let mut temp_key = Vec::with_capacity(data.len() + 1);
         temp_key.push(prefix);
         temp_key.extend_from_slice(data);
         temp_key
     }
 
-    fn account_update_key(&self, owner: &[u8]) -> String {
+    fn account_update_key(cluster: &Cluster, owner: &[u8]) -> String {
         // SAFETY: we don't expect the RPC to provide us invalid pubkeys ever
-        format!("{}:{}", self.cluster, Pubkey::try_from(owner).unwrap())
+        format!("{}:{}", cluster, Pubkey::try_from(owner).unwrap())
     }
 }
 
@@ -168,5 +177,93 @@ impl Drop for KafkaPublisher {
         if let Err(e) = self.producer.flush(self.shutdown_timeout) {
             error!("Failed to flush producer: {}", e);
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::*;
+
+    const PK: &str = "A15Y2eoMNGeX4516TYTaaMErwabCrf9AB9mrzFohdQJz";
+    fn event_with_owner(owner: &Pubkey) -> UpdateAccountEvent {
+        UpdateAccountEvent {
+            slot: 9,
+            pubkey: Pubkey::new_unique().to_bytes().to_vec(),
+            lamports: 100,
+            owner: owner.to_bytes().to_vec(),
+            executable: false,
+            rent_epoch: 0,
+            data: "account data".as_bytes().to_vec(),
+            write_version: 1,
+            txn_signature: None,
+        }
+    }
+
+    #[test]
+    fn account_update_key_and_data_no_wrap() {
+        fn check(cluster: Cluster, expected_key: &str) {
+            let owner = Pubkey::from_str(PK).unwrap();
+            let ev = event_with_owner(&owner);
+            let (key, data) =
+                KafkaPublisher::account_update_key_and_data(ev.clone(), &cluster, false);
+            let key = String::from_utf8_lossy(key.as_slice());
+
+            let mut bytes = data.as_slice();
+            let decoded = UpdateAccountEvent::decode(&mut bytes).unwrap();
+
+            assert_eq!(key, expected_key);
+            assert_eq!(decoded, ev);
+        }
+
+        check(
+            Cluster::Mainnet,
+            "mainnet:A15Y2eoMNGeX4516TYTaaMErwabCrf9AB9mrzFohdQJz",
+        );
+        check(
+            Cluster::Devnet,
+            "devnet:A15Y2eoMNGeX4516TYTaaMErwabCrf9AB9mrzFohdQJz",
+        );
+        check(
+            Cluster::Testnet,
+            "testnet:A15Y2eoMNGeX4516TYTaaMErwabCrf9AB9mrzFohdQJz",
+        );
+    }
+
+    #[test]
+    fn account_update_key_and_data_wrap() {
+        fn check(cluster: Cluster, expected_key: &str) {
+            let owner = Pubkey::from_str(PK).unwrap();
+            let ev = event_with_owner(&owner);
+            let wrapped = MessageWrapper {
+                event_message: Some(EventMessage::Account(Box::new(ev.clone()))),
+            };
+
+            let (key, data) = KafkaPublisher::account_update_key_and_data(ev, &cluster, true);
+
+            assert_eq!(key[0], 65u8);
+            let key = key.into_iter().skip(1).collect::<Vec<_>>();
+            let key = String::from_utf8_lossy(key.as_slice());
+
+            let mut bytes = data.as_slice();
+            let decoded = MessageWrapper::decode(&mut bytes).unwrap();
+
+            assert_eq!(key, expected_key);
+            assert_eq!(decoded, wrapped);
+        }
+
+        check(
+            Cluster::Mainnet,
+            "mainnet:A15Y2eoMNGeX4516TYTaaMErwabCrf9AB9mrzFohdQJz",
+        );
+        check(
+            Cluster::Devnet,
+            "devnet:A15Y2eoMNGeX4516TYTaaMErwabCrf9AB9mrzFohdQJz",
+        );
+        check(
+            Cluster::Testnet,
+            "testnet:A15Y2eoMNGeX4516TYTaaMErwabCrf9AB9mrzFohdQJz",
+        );
     }
 }
